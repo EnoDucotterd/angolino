bbc93893eea0798cfedcb4f1293be2a67a646fca
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-12-07 12:01:32 +0100

    ajout d'un carouselle

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
-const swiper = new Swiper(".swiper-container", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
 });
