6039e695810d8c2197f236f324a362a1110448b4
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-12-14 09:21:53 +0100

    ajustement du javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,31 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const slidesContainer = document.getElementById("slides-container-mobile");
+const slideMobile = document.querySelector(".slide-mobile");
+const prevButtonMobile = document.getElementById("slide-arrow-prev-mobile");
+const nextButtonMobile = document.getElementById("slide-arrow-next-mobile");
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
+nextButtonMobile.addEventListener("click", () => {
+  const slideWidth = slideMobile.clientWidth;
   slidesContainer.scrollLeft += slideWidth;
 });
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
+prevButtonMobile.addEventListener("click", () => {
+  const slideWidth = slideMobile.clientWidth;
   slidesContainer.scrollLeft -= slideWidth;
+});
+
+const slidesContainerDesktop = document.getElementById(
+  "slides-container-desktop"
+);
+const slideDesktop = document.querySelector(".slide-desktop");
+const prevButtonDesktop = document.getElementById("slide-arrow-prev-desktop");
+const nextButtonDesktop = document.getElementById("slide-arrow-next-desktop");
+
+nextButtonDesktop.addEventListener("click", () => {
+  const slideWidth = slideDesktop.clientWidth;
+  slidesContainerDesktop.scrollLeft += slideWidth;
+});
+
+prevButtonDesktop.addEventListener("click", () => {
+  const slideWidth = slideDesktop.clientWidth;
+  slidesContainerDesktop.scrollLeft -= slideWidth;
 });
